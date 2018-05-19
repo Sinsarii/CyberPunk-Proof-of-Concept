b8eeeeb5eecc6507b986a24a4cd1378bb614e73f
Sinsarii <mikolaskin@gmail.com>
2018-05-18 21:21:34 -0400

    removed tests

--- screens/main_menu.gd
+++ screens/main_menu.gd
@@ -2,12 +2,15 @@ extends "abstract_screen.gd"
 
 # class member variables go here, for example:
 # var a = 2
+
 # var b = "textvar"
+
+onready var continue_button = get_node("buttons/continue")
 
 func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
-	pass
+	continue_button.disabled = true
 
 #func _process(delta):
 #	# Called every frame. Delta is time since last frame.
